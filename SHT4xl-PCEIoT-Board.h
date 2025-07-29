65f276aaaaeb5e4128078088f53b279728123f78
Pablo Daniel <137708030+MmonkeyBu@users.noreply.github.com>
2025-07-29 15:28:07 -0300

    Update Precisão

--- SHT4xl-PCEIoT-Board.h
+++ SHT4xl-PCEIoT-Board.h
@@ -6,6 +6,13 @@
 
 #define SHT4XL_I2C_ADRESS 0x44 // Endereço I2c indicado no datasheet do sensor
 
+//Nivel da precisão 
+typedef enum {
+    SHT4X_HIGH_PRECISION,
+    SHT4X_MEDIUM_PRECISION,
+    SHT4X_LOW_PRECISION
+} SHT4x_Precision;
+
 bool sht4xl_init(void);
 bool sht4xl_Ler_TempHum(float *temperature, float *humidity);
 bool sht4xl_reset(void);
