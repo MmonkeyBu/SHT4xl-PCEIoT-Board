5b938072aba335965be7a635c3d7f3e895448f76
Pablo Daniel <137708030+MmonkeyBu@users.noreply.github.com>
2025-07-30 20:15:05 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "SHT4xl-PCEIoT-Board.h" 
+
+int main() {
+
+    stdio_init_all();
+    sleep_ms(2000); 
+
+    if (!sht4x_init()) {
+        printf("Falha ao iniciar o sensor SHT4x. Verifique as conexoes.\n");
+    }
+
+    printf("Sensor SHT4x pronto. Iniciando medicoes...\n");
+
+    float temperatura, umidade;
+    while (1) {
+
+        if (sht4x_read_temp_hum(PRECISION_MEDIUM, &temperatura, &umidade)) {
+            printf("Temperatura: %.2f C, Umidade: %.2f %%RH\n", temperatura, umidade);
+        } else {
+            printf("Erro na leitura do sensor.\n");
+        }
+
+        sleep_ms(2000);
+    }
+
+    return 0;
+}
